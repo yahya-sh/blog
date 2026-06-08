7c6104ca3b7b6945b2f69e123d6572a5701270a4
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-08 15:37:01 +0300

    updated UserAdmin class

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from . import models
+from django.utils.translation import gettext as _
 
 # Register your models here.
 
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "first_name", "last_name", "is_staff")
     ordering = ["email"]
+    fieldsets = (
+        (None, {"fields": ("password",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
