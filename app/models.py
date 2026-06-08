9ae79455b11e8e9c444713953183fab9a490b13d
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-08 09:20:39 +0300

    Config & Define custom user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, AbstractUser, BaseUserManager
+from django.utils.translation import gettext as _
+
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'app.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
